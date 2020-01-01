8e30b9c65e4fa93c3183465f624b518c487ca6e2
M4nt0s <M4nt0s.Youtube@outlook.de>
2020-01-01 12:20:10 +0100

    Add files via upload

---
+++ JumpPadz/README.txt
@@ -0,0 +1,9 @@
+Informationen zum Plugin:
+
+- Welt kann derzeit nicht verändert werden.
+- Block kann derzeit nicht geändert werden.
+- Sprunghöhe kann derzeit nicht geändert werden.
+
+- Block: Slimeblock -> ID: 165
+- Welt: jumppadz
+- Sprunghöhe: ca. 6 Blöcke
---
+++ JumpPadz/src/JumpPadz/Main.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace JumpPadz;
+
+use pocketmine\Player;
+use pocketmine\event\EventListener;
+use pocketmine\entity\Effect;
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\block\Block;
+use pocketmine\math\Vector3;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener{
+
+        public function onEnable(){
+
+                $this->getServer()->getPluginManager()->registerEvents($this, $this);
+                $this->getLogger()->info("JumpPadz wurde erfolgreich gestartet!");
+        }
+
+        public function onDisable(){
+
+                $this->getLogger()->info("JumpPadz wurde erfolgreich beendet!");
+        }
+
+        public function onMove(PlayerMoveEvent $event){
+
+                $player = $event->getPlayer();
+                $y = $player->getY();
+                $block = $event->getPlayer()->getLevel()->getBlock($event->getPlayer()->floor()->subtract(0, 1));
+                if($player->getLevel()->getName() == "jumppadz"){
+
+                        if(block instanceof Block){
+                                
+                                $id = $block->getId();
+                                $meta = $block->getDamage();
+
+                                if($id == 165){
+
+                                        $jumpHeight = 30;
+                                        $height = ($jumpHeight - $y) / 10;
+                                        $player->setMotion(new Vector3(0, 1, 0));8
+                                }
+                        }
+                }
+        }
+}
